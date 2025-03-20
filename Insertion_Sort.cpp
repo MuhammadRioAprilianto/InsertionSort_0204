57dcdd2deeb7dc566f596ec42076585853fd48ef
MuhammadRioAprilianto <rma994636@gmail.com>
2025-03-20 17:08:44 +0700

    membuat prosedur insertionSort

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -32,3 +32,20 @@ void input()
     }
 }
 
+void insertionSort()
+{
+    int temp; //membuat variable data temporer
+    int j, i; //membuat variable j sebagai penanda
+
+    for (int i = 1; i <= n - 1; i++)
+    {// step 1
+        temp = arr[i]; //step 2
+        j = i - 1; //step 3
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr [j + 1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
+        arr[j + 1] = temp; //step 5
+    }
+}
